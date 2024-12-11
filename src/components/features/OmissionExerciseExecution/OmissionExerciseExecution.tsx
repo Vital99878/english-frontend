d2e899d389302651c6a4c53b3c99aacf7d3e7485
Vitaly Likhachev <vitaly99878@gmail.com>
2024-12-11 17:02:10 +0300

    fix: long imports

--- src/components/features/OmissionExerciseExecution/OmissionExerciseExecution.tsx
+++ src/components/features/OmissionExerciseExecution/OmissionExerciseExecution.tsx
@@ -1,73 +1,71 @@
-import { useRef, useState, useEffect } from "react";
-import { BehaviorSubject } from "rxjs";
-import OmissionExerciseService from "@services/omissionExerciseService";
-import AutoCompleteInput from "../../AutoCompleteInput/AutoCompleteInput";
-import Layout from "@components/Layout/index";
+import { useRef, useState } from 'react'
+import { BehaviorSubject } from 'rxjs'
+import OmissionExerciseService from '@services/omissionExerciseService'
+import AutoCompleteInput from '../../AutoCompleteInput/AutoCompleteInput'
+import Layout from '@components/Layout/index'
 
 export default function OmissionExerciseExecution(props: { exercise: string }) {
-  const exercise = new OmissionExerciseService(props.exercise);
-  const solution = useRef<Array<string>>([]);
-  const counter$ = new BehaviorSubject(0);
-  const [isSolutionChecked, setIsSolutionChecked] = useState(false);
-  const [correctAnswerKeys, setCorrectAnswerKeys] = useState<Array<boolean>>(
-    [],
-  );
+    const exercise = new OmissionExerciseService(props.exercise)
+    const solution = useRef<Array<string>>([])
+    const counter$ = new BehaviorSubject(0)
+    const [isSolutionChecked, setIsSolutionChecked] = useState(false)
+    const [correctAnswerKeys, setCorrectAnswerKeys] = useState<Array<boolean>>([])
 
-  function checkSolution(evt: SubmitEvent) {
-    evt.preventDefault();
+    function checkSolution(evt: SubmitEvent) {
+        evt.preventDefault()
 
-    if (exercise.keys.length !== solution.current.length) {
-      console.log("Введите все ответы");
-      // todo всплывашку
-      return;
+        if (exercise.keys.length !== solution.current.length) {
+            console.log('Введите все ответы')
+            // todo всплывашку
+            return
+        }
+
+        setCorrectAnswerKeys(exercise.checkAnswer(solution.current))
+        setIsSolutionChecked(true)
+        const input = document.querySelector<HTMLInputElement>('.isInCorrect')
+        if (input) {
+            input.focus()
+            input.select()
+        }
     }
 
-    setCorrectAnswerKeys(exercise.checkAnswer(solution.current));
-    setIsSolutionChecked(true);
-    const input = document.querySelector<HTMLInputElement>('.isInCorrect')
-    if(input) {
-      input.focus()
-      input.select()
-    }
-  }
+    return (
+        <Layout>
+            <div className={'my-9 flex flex-col gap-5'} style={{ maxWidth: '80ch' }}>
+                <h1>Title</h1>
+                <p>Description</p>
+                <form onSubmit={checkSolution}>
+                    <div className={'exercise'}>
+                        {exercise.valueForUI.map((value, index) => {
+                            if (value === '[key]') {
+                                const Input = (
+                                    <AutoCompleteInput
+                                        key={index}
+                                        options={exercise.keys}
+                                        correctAnswerKeys={correctAnswerKeys}
+                                        solutionData={{ keyPlace: counter$.getValue(), solution }}
+                                        isSolutionChecked={isSolutionChecked}
+                                        setIsSolutionToFalse={() => setIsSolutionChecked(false)}
+                                        inputProps={{ onSubmit: checkSolution, autoFocus: true }}
+                                    />
+                                )
 
-  return (
-    <Layout>
-      <div className={"my-9 flex flex-col gap-5"} style={{ maxWidth: "80ch" }}>
-        <h1>Title</h1>
-        <p>Description</p>
-        <form onSubmit={checkSolution}>
-          <div className={"exercise"}>
-            {exercise.valueForUI.map((value, index) => {
-              if (value === "[key]") {
-                const Input = (
-                  <AutoCompleteInput
-                    key={index}
-                    options={exercise.keys}
-                    correctAnswerKeys={correctAnswerKeys}
-                    solutionData={{ keyPlace: counter$.getValue(), solution }}
-                    isSolutionChecked={isSolutionChecked}
-                    setIsSolutionToFalse={() => setIsSolutionChecked(false)}
-                    inputProps={{ onSubmit: checkSolution, autoFocus: true }}
-                  />
-                );
-
-                counter$.next(counter$.getValue() + 1);
-                return Input;
-              } else {
-                return <span key={index}>{value}</span>;
-              }
-            })}
-          </div>
-          <button
-            tabIndex={2}
-            className={"ml-auto mt-3.5 p-2 border:none bg-transparent"}
-            onClick={() => checkSolution}
-          >
-            Проверить
-          </button>
-        </form>
-      </div>
-    </Layout>
-  );
+                                counter$.next(counter$.getValue() + 1)
+                                return Input
+                            } else {
+                                return <span key={index}>{value}</span>
+                            }
+                        })}
+                    </div>
+                    <button
+                        tabIndex={2}
+                        className={'ml-auto mt-3.5 p-2 border:none bg-transparent'}
+                        onClick={() => checkSolution}
+                    >
+                        Проверить
+                    </button>
+                </form>
+            </div>
+        </Layout>
+    )
 }
--- src/components/features/RightOrderExerciseExecution/OrderExerciseExecution.tsx
+++ src/components/features/RightOrderExerciseExecution/OrderExerciseExecution.tsx
@@ -1,89 +1,64 @@
-import { useState, useMemo } from "react";
-
-import Layout from "@components/Layout/index";
-import style from "./RightOrderExerciseExecution.module.scss";
-
-import {
-  DndContext,
-  closestCenter,
-  useSensors,
-  useSensor,
-  PointerSensor,
-  KeyboardSensor,
-  TouchSensor,
-} from "@dnd-kit/core";
-
-import {
-  SortableContext,
-  arrayMove,
-  sortableKeyboardCoordinates,
-  horizontalListSortingStrategy,
-} from "@dnd-kit/sortable";
-import { SortableItem } from "@features/RightOrderExerciseExecution/components/SortableItem";
-import OrderExerciseService from "@services/orderExerciseService";
+import { useState, useMemo } from 'react'
+import Layout from '@components/Layout/index'
+import style from './RightOrderExerciseExecution.module.scss'
+// prettier-ignore
+import { DndContext, closestCenter, useSensors, useSensor, PointerSensor, KeyboardSensor, TouchSensor } from '@dnd-kit/core'
+// prettier-ignore
+import { SortableContext, arrayMove, sortableKeyboardCoordinates, horizontalListSortingStrategy } from '@dnd-kit/sortable'
+import { SortableItem } from '@features/RightOrderExerciseExecution/components/SortableItem'
+import OrderExerciseService from '@services/orderExerciseService'
 
 type Props = {
-  exercise: string;
-};
+    exercise: string
+}
 
 export default function OrderExerciseExecution({ exercise }: Props) {
-  const orderExercise = useMemo(
-    () => new OrderExerciseService(exercise),
-    [exercise],
-  );
+    const orderExercise = useMemo(() => new OrderExerciseService(exercise), [exercise])
 
-  const [parts, setParts] = useState(orderExercise.exercise);
+    const [parts, setParts] = useState(orderExercise.exercise)
 
-  // @ts-ignore
-  function handleDragEnd(event) {
-    const { active, over } = event;
+    // @ts-ignore
+    function handleDragEnd(event) {
+        const { active, over } = event
 
-    if (active.id !== over?.id) {
-      setParts((items) => {
-        const oldIndex = items.indexOf(active.id);
-        const newIndex = items.indexOf(over.id);
+        if (active.id !== over?.id) {
+            setParts((items) => {
+                const oldIndex = items.indexOf(active.id)
+                const newIndex = items.indexOf(over.id)
 
-        return arrayMove(items, oldIndex, newIndex);
-      });
+                return arrayMove(items, oldIndex, newIndex)
+            })
+        }
     }
-  }
 
-  function checkSolution() {
-    console.log(orderExercise.checkSolution(parts));
-  }
+    function checkSolution() {
+        console.log(orderExercise.checkSolution(parts))
+    }
 
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(TouchSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    }),
-  );
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(TouchSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    )
 
-  return (
-    <Layout>
-      <h1>Упражение номер 1</h1>
-      <p>Составьте предложение</p>
-      <DndContext
-        sensors={sensors}
-        collisionDetection={closestCenter}
-        onDragEnd={handleDragEnd}
-      >
-        <SortableContext items={parts} strategy={horizontalListSortingStrategy}>
-          <p className={style.sentence}>
-            {parts.map((part) => (
-              <SortableItem key={part} part={part} />
-            ))}
-          </p>
-        </SortableContext>
-      </DndContext>
-      <button
-        tabIndex={2}
-        className={"ml-auto mt-3.5 p-2 border:none bg-transparent"}
-        onClick={checkSolution}
-      >
-        Проверить
-      </button>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <h1>Упражение номер 1</h1>
+            <p>Составьте предложение</p>
+            <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+                <SortableContext items={parts} strategy={horizontalListSortingStrategy}>
+                    <p className={style.sentence}>
+                        {parts.map((part) => (
+                            <SortableItem key={part} part={part} />
+                        ))}
+                    </p>
+                </SortableContext>
+            </DndContext>
+            <button tabIndex={2} className={'ml-auto mt-3.5 p-2 border:none bg-transparent'} onClick={checkSolution}>
+                Проверить
+            </button>
+        </Layout>
+    )
 }
