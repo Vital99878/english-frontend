ef2b1ca121bce6442172969b682139b554a73243
Vitaly Likhachev <vitaly99878@gmail.com>
2024-12-11 18:08:07 +0300

    refactoring

--- src/services/orderExerciseService.ts
+++ src/services/orderExerciseService.ts
@@ -1,37 +1,36 @@
-import { OrderExercise } from "@models/orderExercise";
+import { OrderExercise } from '@models/orderExercise'
 
- export default class OrderExerciseService implements OrderExercise {
-  // private exercise: string[] = [];
-  keys: string[] = [];
-  solution: string[] = [];
+export default class OrderExerciseService implements OrderExercise {
+    keys: string[] = []
+    solution: string[] = []
 
-  constructor(exercise: string) {
-    this.createKeys(exercise);
-  }
-
-  public checkSolution(answers: string[]) {
-    return answers.toString() === this.keys.toString();
-  }
-
-  private createKeys(exercise: string): void {
-    const regex = /\[([^\]]+)]/g;
-    let match;
-    while ((match = regex.exec(exercise)) !== null) {
-      this.keys.push(match[1]);
-    }
-    console.log('this.keys created: ', this.keys)
-  }
-
-  get exercise() {
-    const array: string [] = [...this.keys]
-    for (let i = array.length - 1; i > 0; i--) {
-      // Генерируем случайный индекс от 0 до i
-      const randomIndex = Math.floor(Math.random() * (i + 1));
-
-      // Меняем местами текущий элемент и случайный
-      [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+    constructor(exercise: string) {
+        this.createKeys(exercise)
     }
 
-    return array;
-  }
+    public checkSolution(answers: string[]) {
+        return answers.toString() === this.keys.toString()
+    }
+
+    private createKeys(exercise: string): void {
+        const regex = /\[([^\]]+)]/g
+        let match
+        while ((match = regex.exec(exercise)) !== null) {
+            this.keys.push(match[1])
+        }
+    }
+
+    /**
+     * Возвращает куски предложения в хаотичном порядке
+     */
+    get exercise(): string[] {
+        const array: string[] = [...this.keys]
+        for (let i = array.length - 1; i > 0; i--) {
+            const randomIndex = Math.floor(Math.random() * (i + 1))
+
+            ;[array[i], array[randomIndex]] = [array[randomIndex], array[i]]
+        }
+
+        return array
+    }
 }
