9cbe54cba33abb032524a2cbbb85fc35c7162ce5
Vitaly Likhachev <vitaly99878@gmail.com>
2024-12-17 12:51:43 +0300

    feat(snackbar) тестовая всплывашка

--- src/const/exercise_with_omissions.ts
+++ src/const/exercise_with_omissions.ts
@@ -2,19 +2,7 @@ export const EXERCISE_WITH_OMISSIONS =
     'The sun was setting over the [ ], [the] a warm [ ] glow across the [the] as the day gradually faded twilight.'
 
 export const EXERCISE_WITH_OMISSIONS_2 =
-    'This is [a] pen. That is [a] pencil. 2. This is [a] book.\n' +
-    'It is  my [ ] book. 3. Is this [ ] your pencil? — No, it isn’t my\n' +
-    '[ ] pencil, it is my [ ] sister’s pencil. 4. I have [a] sister. My\n' +
-    'sister is [an] engineer. My sister’s husband is [a] doctor.\n' +
-    'They have got two children. 5. This is [a] girl. This girl is\n' +
-    'their [ ] daughter. Their daughter’s name is Vera. 6. That\n' +
-    'is a boy. That boy is her [ ] brother. Her brother’s name\n' +
-    'is Nick. 7. This is our flat. 8. We have got [a] car. Our\n' +
-    'car is not very expensive but reliable. 9. I have no [ ] pet.\n' +
-    '10. My granny has got [a] headache. She has no idea\n' +
-    'what to do. 11. I have [a] friend. His name is Mike. My\n' +
-    '[ ] friend is very good. 12. It’s [a] cat. Its tail is long and\n' +
-    'bushy. It’s Mike’s [ ] cat.'
+    '1. This is [a] pen. That is [a] pencil. 2. This is [a] book. It is my [ ] book. 3. Is this your [ ] pencil? — No, it isn’t my [ ] pencil, it is my sister’s [ ] pencil. 4. I have [a] sister. My [ ] sister is [an] engineer. My sister’s [ ] husband is [a] doctor. They have got two [ ] children. 5. This is [a] girl. This [ ] girl is their [ ] daughter. Their daughter’s [ ] name is Vera. 6. That is [a] boy. That [ ] boy is her [ ] brother. Her brother’s [ ] name is Nick. 7. This is our [ ] flat. 8. We have got [a] car. Our [ ] car is not very expensive but reliable. 9. I have no [ ] pet. 10. My granny has got [a] headache. She has no [ ] idea what to do. 11. I have [a] friend. His [ ] name is Mike. My [ ] friend is very good. 12. It’s [a] cat. Its [ ] tail is long and bushy. It’s Mike’s [ ] cat.\n'
 
 export const EXERCISE_RIGHT_ORDER = '[1-some text] [2-with new]  [3-order list]'
 
