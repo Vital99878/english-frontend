4f804d7eacc9c81721778686068f824f6e878ae6
Vitaly Likhachev <vitaly99878@gmail.com>
2025-01-08 23:18:12 +0300

    fix(types) - fix stale time for queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
-import { QueryClient } from '@tanstack/react-query'
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {QueryClient} from '@tanstack/react-query'
+import {PersistQueryClientProvider} from '@tanstack/react-query-persist-client'
+import {createSyncStoragePersister} from '@tanstack/query-sync-storage-persister'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import ExercisePage from '@pages/ExercisePage'
 
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
+            staleTime: 1000 * 60 * 60 * 3,
             gcTime: 1000 * 60 * 60 * 24, // 24 hours
         },
     },
