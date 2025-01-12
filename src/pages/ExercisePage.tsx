944573fb4f37410d0d09d3a7f5f55b41ba7cac1d
Vitaly Likhachev <vitaly99878@gmail.com>
2025-01-12 23:10:44 +0300

    mock - deploy. Сделал моки в ExercisePage.tsx

--- src/pages/ExercisePage.tsx
+++ src/pages/ExercisePage.tsx
@@ -6,30 +6,18 @@ import { useState, useEffect } from 'react'
 
 export default function ExercisePage() {
     const location = useLocation()
-    const [, , exerciseNumber] = location.pathname.split('/')
+    const [, , theme, exerciseNumber] = location.pathname.split('/')
 
     const [mockExercise, setMockExercise] = useState<IExercise<'omissions'> | null | undefined>(null)
-
-    // const { data, isFetching } = useQuery({
-    //     queryKey: [`theme ${theme}`, `exercise number: ${exerciseNumber}`],
-    //     queryFn: async () => {
-    //         if (MOCK_ENABLED) {
-    //             console.log('MOCK_ENABLED: ', MOCK_ENABLED)
-    //             const articles = await getJSON<Array<IExercise<'omissions'>>>('exercises')
-    //             return articles.find((e) => e.id === Number(exerciseNumber))
-    //         } else {
-    //             return api.getExercise(theme, exerciseNumber)
-    //         }
-    //     },
-    // })
 
     useEffect(() => {
         async function getMockExercise() {
             const articles = await getJSON<Array<IExercise<'omissions'>>>('exercises')
-            setMockExercise(articles.find((e) => e.id === Number(exerciseNumber)))
+            console.log(theme, exerciseNumber)
+            return articles.find((e) => e.id === Number(exerciseNumber))
         }
-        getMockExercise().then(console.log)
-    }, [exerciseNumber])
+        getMockExercise().then(setMockExercise)
+    }, [exerciseNumber, theme])
 
     if (!mockExercise) {
         return <div>is Fetching ...</div>
