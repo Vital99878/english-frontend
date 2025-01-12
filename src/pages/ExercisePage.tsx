6d63fb0bb18ddd5ffaff2bbda49de88aa6c1f7fa
Vitaly Likhachev <vitaly99878@gmail.com>
2025-01-12 22:57:05 +0300

    mock - deploy. Сделал моки в ExercisePage.tsx

--- src/pages/ExercisePage.tsx
+++ src/pages/ExercisePage.tsx
@@ -1,23 +1,39 @@
 import { useLocation } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
-import api from '@services/apiService'
-import ShowExerciseByType from "@src/hoc/ShowExerciseByType";
+import ShowExerciseByType from '@src/hoc/ShowExerciseByType'
+import getJSON from '@mock/getJSON'
+import { IExercise } from '@models/IExercise'
+import { useState, useEffect } from 'react'
 
 export default function ExercisePage() {
     const location = useLocation()
-    const [, ,theme, exerciseNumber] = location.pathname.split('/')
+    const [, , exerciseNumber] = location.pathname.split('/')
 
-    const { data, isFetching } = useQuery({
-        queryKey: [`theme ${theme}`, `exercise number: ${exerciseNumber}`],
-        queryFn: () => api.getExercise(theme, exerciseNumber),
-    })
+    const [mockExercise, setMockExercise] = useState<IExercise<'omissions'> | null | undefined>(null)
 
-    if(!data?.data) {
-        return null
-    }
-    if (isFetching) {
+    // const { data, isFetching } = useQuery({
+    //     queryKey: [`theme ${theme}`, `exercise number: ${exerciseNumber}`],
+    //     queryFn: async () => {
+    //         if (MOCK_ENABLED) {
+    //             console.log('MOCK_ENABLED: ', MOCK_ENABLED)
+    //             const articles = await getJSON<Array<IExercise<'omissions'>>>('exercises')
+    //             return articles.find((e) => e.id === Number(exerciseNumber))
+    //         } else {
+    //             return api.getExercise(theme, exerciseNumber)
+    //         }
+    //     },
+    // })
+
+    useEffect(() => {
+        async function getMockExercise() {
+            const articles = await getJSON<Array<IExercise<'omissions'>>>('exercises')
+            setMockExercise(articles.find((e) => e.id === Number(exerciseNumber)))
+        }
+        getMockExercise().then(console.log)
+    }, [exerciseNumber])
+
+    if (!mockExercise) {
         return <div>is Fetching ...</div>
     }
 
-    return <ShowExerciseByType  {...data} />
+    return <ShowExerciseByType {...mockExercise} />
 }
