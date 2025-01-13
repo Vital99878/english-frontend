436bc422f94ebac97bfda4ad7187d279047dc1df
Vitaly Likhachev <vitaly99878@gmail.com>
2025-01-13 22:34:35 +0300

    mock - mock as const

--- src/pages/ExercisePage.tsx
+++ src/pages/ExercisePage.tsx
@@ -1,24 +1,22 @@
 import { useLocation } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
-import api from '@services/apiService'
-import ShowExerciseByType from "@src/hoc/ShowExerciseByType";
+import ShowExerciseByType from '@src/hoc/ShowExerciseByType'
+import EXERCISES_MOCK from '@mock/response/exercises'
 
 export default function ExercisePage() {
     const location = useLocation()
-    const [, ,theme, exerciseNumber] = location.pathname.split('/')
+    const [, , theme, exerciseNumber] = location.pathname.split('/')
+    console.log(theme, exerciseNumber)
+
+    const data = EXERCISES_MOCK.find((e) => e.id === Number(exerciseNumber))
 
     // console.log(location.pathname.split('/'))
-    const { data, isFetching } = useQuery({
-        queryKey: [`theme ${theme}`, `exercise number: ${exerciseNumber}`],
-        queryFn: () => api.getExercise(theme, exerciseNumber),
-    })
 
-    if(!data?.data) {
+    if (!data?.data) {
         return null
     }
-    if (isFetching) {
-        return <div>is Fetching ...</div>
-    }
+    // if (isFetching) {
+    //     return <div>is Fetching ...</div>
+    // }
 
-    return <ShowExerciseByType  {...data} />
+    return <ShowExerciseByType {...data} />
 }
